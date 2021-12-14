bf3bd729b53dddee22bb7016837b0817b984281b
jSUNSH1NEw <weertz.joffrey@protonmail.ch>
2021-12-14 15:33:18 +0100

    Creating css design and  navBar

--- client/src/view/Landing/components/Header/index.js
+++ client/src/view/Landing/components/Header/index.js
@@ -2,9 +2,11 @@ import React, { useState } from "react";
 import "./header.css";
 import { Icon, Link, Box, Popover, Fade } from '@chakra-ui/react';
 
-import { ReactComponent as WRLDIcon } from "../../../../assets/icon/wrld-icon-black.svg";
+import { ReactComponent as WRLDIconss } from "../../../../assets/icon/WRLD-white.svg";
 import { ReactComponent as GithubIcon } from "../../../../assets/icon/github-icon.svg";
 import { ReactComponent as TwitterIcon } from "../../../../assets/icon/twitter-icon.svg";
+
+import WRLDIcon from "../../../../assets/icon/coin.png";
 
 // BY component={} in SvgIcon, it will render the SVG file as a React component.
 // https://chakra-ui.com/docs/svg-icon
@@ -23,22 +25,25 @@ function Header() {
     return (
 
         <div className="landing-header">
-            <div> <Navbar /> </div>
-            <WRLDIcon color="primary"  viewBox="0 0 174 40" style={{ minWidth: 174, minHeight: 40 }} />
+            <div>  </div>
+            <img src={WRLDIcon} className="landing-header-logo" alt="logo" />
             <div className="landing-header-nav-wrap">
+
                 <Box component="div" onMouseEnter={e => handleClick(e)} onMouseLeave={e => handleClick(e)}>
-                    <p className="landing-header-nav-text">Social</p>
+                <p className="landing-header-nav-text">ROADMAP</p>
+                <p className="landing-header-nav-text">OUR TEAM</p>
+                    <p className="landing-header-nav-text">SOCIAL</p>
                     <Popover className="landing-header-poper" open={open} anchorEl={anchorEl} transition>
                         {({ TransitionProps }) => (
                             <Fade {...TransitionProps} timeout={200}>
                                 <div className="tooltip">
                                     <Link className="tooltip-item" href="https://github.com/jSUNSH1NEw/MoralisAvalanchehackathon/tree/main/client" target="_blank">
                                         <Icon color="primary" component={GithubIcon} />
-                                        <p>GitHub</p>
+                                        <p>GITHUB</p>
                                     </Link>
                                     <Link className="tooltip-item" href="https://twitter.com/wrld_token" target="_blank">
                                         <Icon color="primary" component={TwitterIcon} />
-                                        <p>Twitter</p>
+                                        <p>TWITTER</p>
                                     </Link>
                                 </div>
                             </Fade>
--- client/src/view/Landing/components/Main/index.js
+++ client/src/view/Landing/components/Main/index.js
@@ -1,37 +1,37 @@
 import React from "react";
 import { Link } from '@chakra-ui/react';
 import "./Main.css";
-import { ReactComponent as WRLDIcon } from "../../../../assets/icon/WRLD-white.svg";
+import WRLDIcon  from "../../../../assets/icon/LogoDesigne.png";
 
 function Main() {
     return (
         <div className="landing-main">
             <div className="landing-main-img-wrap">
-                <WRLDIcon className="landing-main-title-icon" />
+            <img src={WRLDIcon} alt="World Protocol logo" />
             </div>
 
             <div className="landing-main-help-text-wrap">
-                <p>We help the brave build  safely their</p>
-                <p>assets with legendary  compound</p>
-                <p>interest</p>
+                <p>WE HELP THE BRAVE BUILD SAFELY THEIR</p>
+                <p>ASSETS WITH LEGENDARY COMPOUND</p>
+                <p>INTEREST</p>
             </div>
 
             <div className="landing-main-btns-wrap">
                 <Link href="https://WorldMillions.gitbook.io/WorldFinance/" target="_blank" rel="noreferrer">
                     <div className="landing-main-btn">
-                        <p>Our team</p>
+                        <p>HOW IT WORK</p>
                     </div>
                 </Link>
                 
                 <Link href="https://WorldMillions.gitbook.io/WorldFinance/" target="_blank" rel="noreferrer">
                     <div className="landing-main-btn">
-                        <p>Documentation</p>
+                        <p>DOCUMENTATIONS</p>
                     </div>
                 </Link>
 
                 <Link href="" target="_blank" rel="noreferrer">
                     <div className="landing-main-btn">
-                        <p>Entry ticket</p>
+                        <p>ENTRY TICKET</p>
                     </div>
                 </Link>
             </div>
