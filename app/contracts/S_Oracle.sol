b975975b760111dd4440b54e030d1db5fc9ee6a5
tomguluson92 <314913739@qq.com>
2021-12-23 11:42:17 +0800

    Add files via upload
    2021_12_23 solidity for mint, staking reward, oracle and withdraw, solidity begin with S, Samuel

---
+++ app/contracts/S_Oracle.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+// @notice:  get off-chain price to instruct the mint or burn on WRLD token and its corresponding Bond or Staking.
+// samuel ko
+
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+
+contract PriceConsumerV3 {
+
+    AggregatorV3Interface internal priceFeed;
+
+    /**
+     * Network: Kovan (Testnet, Joffery you can change it to AVAX test net snowman or etc to get off-chain prices)
+     * Aggregator: ETH/USD
+     * Address: 0x9326BFA02ADD2366b30bacB125260Af641031331
+     */
+    constructor() {
+        priceFeed = AggregatorV3Interface(0x9326BFA02ADD2366b30bacB125260Af641031331);
+    }
+
+    /**
+     * Returns the latest price
+     */
+    function getLatestPrice() public view returns (int) {
+        (
+            uint80 roundID,
+            int price,
+            uint startedAt,
+            uint timeStamp,
+            uint80 answeredInRound
+        ) = priceFeed.latestRoundData();
+        return price;
+    }
+}
---
+++ app/contracts/S_Token.sol
@@ -0,0 +1,92 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.2;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Snapshot.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20FlashMint.sol";
+
+/// @custom:security-contact samuel.gao023@gmail.com
+contract WorldMillion is ERC20, ERC20Burnable, ERC20Snapshot, Ownable, ERC20FlashMint {
+
+    address public immutable OWNER_ADDRESS;
+    // Bond Name.
+    address public BOND_ONE;
+    address public BOND_TWO;
+
+    constructor() ERC20("WorldMillion", "WRLD") {
+        _mint(msg.sender, 100000 * 10 ** decimals());
+        OWNER_ADDRESS = msg.sender;
+    }
+
+    function snapshot() public onlyOwner {
+        _snapshot();
+    }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+
+    function burn(address from, uint256 amount) public onlyOwner {
+        _burn(from, amount);
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function _beforeTokenTransfer(address from, address to, uint256 amount)
+        internal
+        override(ERC20, ERC20Snapshot)
+    {
+        super._beforeTokenTransfer(from, to, amount);
+    }
+
+    // This function is for linking bond with WRLD token.
+    function bond_link(address bond_address) public onlyOwner {
+        BOND_ONE = bond_address;
+    }
+}
+
+contract WRLDBondOne is ERC20, ERC20Burnable, Ownable {
+
+    constructor() ERC20("WorldMillionBondOne", "WRLDBondOne") {
+        _mint(msg.sender, 100000 * 10 ** decimals());
+    }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+
+    function burn(address from, uint256 amount) public onlyOwner {
+        _burn(from, amount);
+    }
+
+    // TODO: The details are depend on you, msg.sender may be not right here.
+    //      1) mint can be done from the owner of WRLD.
+    //      2) burn?? Do we burn the money of the owner of WRLD? or burn specified account with given number of token?
+    function MintOrBurn(IERC20 wrld) external {
+
+        if (wrld.totalSupply() > msg.sender.totalSupply()) {
+            msg.sender.mint(msg.sender, wrld.totalSupply() - msg.sender.totalSupply());
+        }
+        else if (wrld.totalSupply() < msg.sender.totalSupply()) {
+            msg.sender.burn(_wrld, msg.sender.totalSupply()- wrld.totalSupply());
+        }
+
+    }
+}
+
+contract WRLDStakeOne is ERC20, ERC20Burnable, Ownable {
+
+    constructor() ERC20("WorldMillionBondStaking", "SWRLD") {
+        _mint(msg.sender, 100000 * 10 ** decimals());
+    }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+
+    function burn(address from, uint256 amount) public onlyOwner {
+        _burn(from, amount);
+    }
+}
