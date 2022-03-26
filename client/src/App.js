dc2c32ab0f07651225823c06aaa5077fd7fe1097
jSUNSH1NEw <weertz.joffrey@protonmail.ch>
2022-03-26 16:40:50 +0100

    update architecture of pricate sales landing

--- client/src/App.js
+++ client/src/App.js
@@ -8,7 +8,7 @@ import Landing from "./view/Landing/landing";
 import CreatePassport from "./view/NftsPassport/createPassport/passport";
 import BuyPassport from "./view/NftsPassport/buyPassport/buyPassport";
 import AlreadyHave from "./view/NftsPassport/buyPassport/havePassport";
-//import HavePassport from "./view/havePassport/havePassport";
+import PrivateSale from "view/privateSales/privateSaleLanding";
 import Dashboard from "./view/dappDashboard/dashboard";
 import P404 from "./view/404/404";
 
@@ -20,6 +20,7 @@ function App() {
       <Router>
         <Routes>
           <Route exact path="/" element={<Landing />} />
+
           <Route path="/createPassport" element={<CreatePassport />} />
           <Route path="/getPassport" element={<BuyPassport />} />
           <Route path="/havePassport" element={<AlreadyHave />} />
